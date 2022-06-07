313c46909072644a1a4edc3c379e54b7c60ec3d0
PhoenixSUSU <47848536+PhoenixSUSU@users.noreply.github.com>
2022-06-07 18:32:01 +0500

    Delete del_setup.php

--- del_setup.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "database.php";
-
-$id_del_setup = $_GET["id_del_setup"];
-$sql = "DELETE FROM `history` WHERE id = '$id_del_setup'";
-mysqli_query($link, $sql);
-
-header("Location: ../parea.php");
